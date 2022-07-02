e4210ca5e05b31be2a15a7a810858d2d939fef88
Mahir Mac <mhrsl07@gmail.com>
2022-07-02 18:58:18 +0300

    fix styling issues

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Styles from "./Page.module.scss";
 import classNames from "classnames/bind";
+import { motion } from "framer-motion";
 
 let cx = classNames.bind(Styles);
 
 function Page({ children, title, desc, direction }) {
   return (
-    <div className={Styles.page}>
-      <div className={Styles.page_top}>
+    <motion.div className={Styles.page}>
+      <motion.div className={Styles.page_top}>
         <h2>{title}</h2>
         <h4>{desc}</h4>
-      </div>
-      <div className={Styles.page_bottom}>{children}</div>
-    </div>
+      </motion.div>
+      <motion.div className={Styles.page_bottom}>{children}</motion.div>
+    </motion.div>
   );
 }
 
